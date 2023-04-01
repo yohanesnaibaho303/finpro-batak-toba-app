b9785c9b00f735759c90a458120846ffa949ebbc
Yohanes Pratama Naibaho <76932074+yohanesnaibaho303@users.noreply.github.com>
2023-04-01 21:47:47 +0700

    initialized the project: build UI & set up fuse.js algorithm for dictionary

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="flex-none flex items-center justify-center py-5 px-4 sm:px-6 lg:px-16">
+        <div class="text-gray-400 text-xs text-center">
+      &copy;Bataknese App {{ new Date().getFullYear() }} v01.0.2 <br/>
+      Yohanes Naibaho - President University
+    </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
